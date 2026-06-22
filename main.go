e8984529c3790cda6a7ef8ff13d6d8286fdfc34a
ProtonKicker <tdxt@aliyun.com>
2026-06-22 01:12:27 -0700

    Add pink color rendering with title/footer

--- main.go
+++ main.go
@@ -13,12 +13,41 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Pink background
+	fmt.Print("\033[48;5;225m")
+	fmt.Print("\033[2J\033[H")
+
+	// Title
+	fmt.Println("\033[38;5;206m")
+	fmt.Println("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	fmt.Println("         🐙  Welcome to the Octopus Terminal  🐙")
+	fmt.Println("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	fmt.Println("\033[0m")
+
 	lines := strings.Split(string(data), "\n")
 	for _, line := range lines {
 		if strings.TrimSpace(line) == "" {
 			fmt.Println()
 			continue
 		}
-		fmt.Println("\033[38;5;255m" + line + "\033[0m")
+		// Color each character
+		for _, ch := range line {
+			switch ch {
+			case '█':
+				fmt.Print("\033[38;5;0m██") // black eyes
+			case '▓':
+				fmt.Print("\033[38;5;213m▓▓") // dark pink tentacle tips
+			case '▒':
+				fmt.Print("\033[38;5;206m▒▒") // light pink body
+			default:
+				fmt.Print(string(ch))
+			}
+		}
+		fmt.Println()
 	}
+
+	fmt.Println()
+	fmt.Println("\033[38;5;206m  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+	fmt.Println("            Have a squishy day! 🩷")
+	fmt.Println("  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0m")
 }
